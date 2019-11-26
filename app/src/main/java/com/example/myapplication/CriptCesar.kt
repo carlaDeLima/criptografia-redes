c4a007aab478fa552e20d9b33920ddfa1a7de57c
Carla de Lima <carlas.delimad@gmail.com>
2019-11-25 21:29:44 -0300

    first commit

---
+++ app/src/main/java/com/example/myapplication/CriptCesar.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+class CriptCesar {
+
+
+
+    fun cript() {
+
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val RESULT_LOAD_IMAGE = 1
+    }
+
+    private var cesarClick = false
+    private var cripClick = false
+    private var sDesClick = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        uploadButton.setOnClickListener {
+            //cesarClick
+            val i = Intent(
+                Intent.ACTION_PICK,
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+            )
+            startActivityForResult(i, RESULT_LOAD_IMAGE)
+        }
+
+        cesarButton.setOnClickListener {
+            cesarClick = if (cesarClick) {
+                cripClick = false
+                cripButton.isEnabled = true
+
+                sDesClick = false
+                s_des_Button.isEnabled = true
+                false
+            } else {
+                cripClick = false
+                cripButton.isEnabled = false
+
+                sDesClick = false
+                s_des_Button.isEnabled = false
+                true
+            }
+        }
+        cripButton.setOnClickListener {
+            cripClick = if (cripClick) {
+                cesarClick = false
+                cesarButton.isEnabled = true
+
+                sDesClick = false
+                s_des_Button.isEnabled = true
+                false
+            } else {
+                cesarClick = false
+                cesarButton.isEnabled = false
+
+                sDesClick = false
+                s_des_Button.isEnabled = false
+                true
+            }
+        }
+        s_des_Button.setOnClickListener {
+            sDesClick = if (sDesClick) {
+                cesarClick = false
+                cesarButton.isEnabled = true
+
+                cripClick = false
+                cripButton.isEnabled = true
+                false
+            } else {
+                cesarClick = false
+                cesarButton.isEnabled = false
+
+                cripClick = false
+                cripButton.isEnabled = false
+                true
+            }
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            val selectedImage = data.data
+            val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
+            val cursor = contentResolver.query(selectedImage!!, filePathColumn, null, null, null)
+            cursor!!.moveToFirst()
+            val columnIndex = cursor.getColumnIndex(filePathColumn[0])
+            val picturePath = cursor.getString(columnIndex)
+            cursor.close()
+            imageUpload.setImageBitmap(BitmapFactory.decodeFile(picturePath))
+            var teste = BitmapFactory.decodeFile(picturePath)
+
+        }
+    }
+}
